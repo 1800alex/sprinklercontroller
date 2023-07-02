de998d1881c4b6fcf5ac9e85b5b0252231b405e0
aeaton <aeaton@batswireless.com>
2023-07-02 10:51:26 -0400

    Initial add

--- src/SprinklerHeadController.h
+++ src/SprinklerHeadController.h
@@ -6,25 +6,27 @@
 typedef void (*TogglePumpFunction)(void *, bool);
 typedef void (*ToggleHeadFunction)(void *, uint8_t, bool);
 
-typedef struct {
-  uint8_t NumHeads;
-  TogglePumpFunction TogglePumpFunction;
-  int PumpDelay;
-  ToggleHeadFunction ToggleHeadFunction;
-  int HeadOnTime;
-  int HeadOffTime;
+typedef struct
+{
+	uint8_t NumHeads;
+	TogglePumpFunction TogglePumpFunction;
+	int PumpDelay;
+	ToggleHeadFunction ToggleHeadFunction;
+	int HeadOnTime;
+	int HeadOffTime;
 } SprinklerHeadControllerOptions;
 
-typedef struct {
-  int cycle;
-  uint8_t numHeads;
-  TogglePumpFunction togglePumpFunction;
-  int pumpDelay;
-  ToggleHeadFunction toggleHeadFunction;
-  int headOnTime;
-  int headOffTime;
-  uint8_t nextStartIndex;
-  uint8_t *ledState;
+typedef struct
+{
+	int cycle;
+	uint8_t numHeads;
+	TogglePumpFunction togglePumpFunction;
+	int pumpDelay;
+	ToggleHeadFunction toggleHeadFunction;
+	int headOnTime;
+	int headOffTime;
+	uint8_t nextStartIndex;
+	uint8_t *ledState;
 } SprinklerHeadController;
 
 SprinklerHeadController *SprinklerHeadControllerNew(SprinklerHeadControllerOptions opts);
--- src/main.c
+++ src/main.c
@@ -11,84 +11,94 @@
 
 #include <ncurses.h>
 
-void displayCycle(SprinklerHeadController *controller) {
-  move(1, 1);
-  printw("Cycle %d", controller->cycle + 1);
+void displayCycle(SprinklerHeadController *controller)
+{
+	move(1, 1);
+	printw("Cycle %d", controller->cycle + 1);
 }
 
-void displayPumpStatus(SprinklerHeadController *controller, bool on) {
-  move(2, 1);
-  printw("Pump: %s", on ? "ON " : "OFF");
+void displayPumpStatus(SprinklerHeadController *controller, bool on)
+{
+	move(2, 1);
+	printw("Pump: %s", on ? "ON " : "OFF");
 }
 
 void displayHeadStatus(SprinklerHeadController *controller, uint8_t index,
-                       bool on) {
-  move(index + 3, 1); // Move to line (index + 1), column 1
-  printw("Head %d: %s", index, on ? "ON " : "OFF");
+	bool on)
+{
+	move(index + 3, 1); // Move to line (index + 1), column 1
+	printw("Head %d: %s", index, on ? "ON " : "OFF");
 }
 
-void displaySetup(SprinklerHeadController *controller) {
-  // Initialize ncurses
-  initscr();
-  clear();
+void displaySetup(SprinklerHeadController *controller)
+{
+	// Initialize ncurses
+	initscr();
+	clear();
 
-  move(0, 1);
-  printw("Sprinkler controller compiled %s %s", __DATE__, __TIME__);
+	move(0, 1);
+	printw("Sprinkler controller compiled %s %s", __DATE__, __TIME__);
 
-  // Display initial states
-  displayCycle(controller);
-  displayPumpStatus(controller, false);
-  for (uint8_t i = 0; i < controller->numHeads; ++i) {
-    displayHeadStatus(controller, i, false);
-  }
-  refresh();
+	// Display initial states
+	displayCycle(controller);
+	displayPumpStatus(controller, false);
+	for(uint8_t i = 0; i < controller->numHeads; ++i)
+	{
+		displayHeadStatus(controller, i, false);
+	}
+	refresh();
 }
 
 void displayEnd(SprinklerHeadController *controller) { endwin(); }
 
-void togglePump(void *this, bool on) {
-  SprinklerHeadController *controller = (SprinklerHeadController *)this;
-  displayPumpStatus(controller, on);
-  refresh();
+void togglePump(void *this, bool on)
+{
+	SprinklerHeadController *controller = (SprinklerHeadController *)this;
+	displayPumpStatus(controller, on);
+	refresh();
 }
 
-void toggleHead(void *this, uint8_t index, bool on) {
-  SprinklerHeadController *controller = (SprinklerHeadController *)this;
-  displayHeadStatus(controller, index, on);
-  refresh();
+void toggleHead(void *this, uint8_t index, bool on)
+{
+	SprinklerHeadController *controller = (SprinklerHeadController *)this;
+	displayHeadStatus(controller, index, on);
+	refresh();
 }
 
 #endif
 
-int main() {
+int main()
+{
 #ifdef PICO
-  stdio_init_all();
-  std::cout << "Running on Pico" << std::endl;
+	stdio_init_all();
+	std::cout << "Running on Pico" << std::endl;
 #endif
 
 #ifdef LINUX
-  SprinklerHeadControllerOptions opts = {.NumHeads = 3,
-                                         .HeadOnTime = 1500,
-                                         .HeadOffTime = 500,
-                                         .ToggleHeadFunction = toggleHead,
-                                         .TogglePumpFunction = togglePump,
-                                         .PumpDelay = 500};
-  SprinklerHeadController *controller = SprinklerHeadControllerNew(opts);
+	SprinklerHeadControllerOptions opts = {.NumHeads = 3,
+		.HeadOnTime = 1500,
+		.HeadOffTime = 500,
+		.ToggleHeadFunction = toggleHead,
+		.TogglePumpFunction = togglePump,
+		.PumpDelay = 500};
+	SprinklerHeadController *controller = SprinklerHeadControllerNew(opts);
 
-  if (controller == NULL) {
-    return -1;
-  }
+	if(controller == NULL)
+	{
+		return -1;
+	}
 
-  displaySetup(controller);
+	displaySetup(controller);
 
-  while (true) {
-    displayCycle(controller);
-    SprinklerHeadControllerCycle(controller);
-  }
+	while(true)
+	{
+		displayCycle(controller);
+		SprinklerHeadControllerCycle(controller);
+	}
 
-  displayEnd(controller);
-  SprinklerHeadControllerFree(controller);
+	displayEnd(controller);
+	SprinklerHeadControllerFree(controller);
 #endif
 
-  return 0;
+	return 0;
 }
